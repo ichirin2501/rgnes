3e438606c6decc67aed33841af3edc2884e22f9e
ichirin2501 <ichirin.2501@gmail.com>
2020-03-18 22:09:08 +0900

    fix

--- nes/instruction.go
+++ nes/instruction.go
@@ -21,8 +21,8 @@ const (
 type instruction int
 
 const (
-	UNKNOWN instruction = iota
-	LDA
+	unknownInstruction instruction = iota
+	lda
 )
 
 type opcode struct {
@@ -33,12 +33,12 @@ type opcode struct {
 }
 
 var m = map[byte]opcode{
-	0xA9: opcode{Name: LDA, Mode: immediate, Size: 2, Cycle: 2},
-	0xA5: opcode{Name: LDA, Mode: zeroPage, Size: 2, Cycle: 3},
-	0xB5: opcode{Name: LDA, Mode: zeroPageX, Size: 2, Cycle: 4},
-	0xAD: opcode{Name: LDA, Mode: absolute, Size: 3, Cycle: 4},
-	0xBD: opcode{Name: LDA, Mode: absoluteX, Size: 3, Cycle: 4},
-	0xB9: opcode{Name: LDA, Mode: absoluteY, Size: 3, Cycle: 4},
-	0xA1: opcode{Name: LDA, Mode: indexedIndirect, Size: 2, Cycle: 6},
-	0xB1: opcode{Name: LDA, Mode: indirectIndexed, Size: 2, Cycle: 5},
+	0xA9: opcode{Name: lda, Mode: immediate, Size: 2, Cycle: 2},
+	0xA5: opcode{Name: lda, Mode: zeroPage, Size: 2, Cycle: 3},
+	0xB5: opcode{Name: lda, Mode: zeroPageX, Size: 2, Cycle: 4},
+	0xAD: opcode{Name: lda, Mode: absolute, Size: 3, Cycle: 4},
+	0xBD: opcode{Name: lda, Mode: absoluteX, Size: 3, Cycle: 4},
+	0xB9: opcode{Name: lda, Mode: absoluteY, Size: 3, Cycle: 4},
+	0xA1: opcode{Name: lda, Mode: indexedIndirect, Size: 2, Cycle: 6},
+	0xB1: opcode{Name: lda, Mode: indirectIndexed, Size: 2, Cycle: 5},
 }
