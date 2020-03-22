66f726da6d4074803ba1a3f698f96142d3824b8e
ichirin2501 <ichirin.2501@gmail.com>
2020-03-22 17:04:03 +0900

    impl step

--- nes/cpu.go
+++ nes/cpu.go
@@ -1,5 +1,7 @@
 package nes
 
+import "fmt"
+
 type CPUCycle struct {
 	stall  int
 	cycles int
@@ -63,16 +65,150 @@ func (cpu *CPU) Step() int {
 		cpu.interrupt.DeassertIRQ()
 	}
 
-	// 色々TODO
+	opcodeByte := fetch(cpu.r, cpu.memory)
+	opcode, ok := opcodeMap[opcodeByte]
+	if !ok {
+		panic(fmt.Sprintf("Unknown opcode: %0x", opcodeByte))
+	}
+	additionalCycle := 0
+	addr, pageCrossed := fetchOperand(cpu.r, cpu.memory, opcode.Mode)
+	if pageCrossed {
+		additionalCycle++
+	}
 
-	// opcodeByte := fetch(cpu.r, cpu.memory)
-	// opcode := opcodeMap[opcodeByte]
+	switch opcode.Name {
+	case LDA:
+		lda(cpu.r, cpu.memory, addr)
+	case LDX:
+		ldx(cpu.r, cpu.memory, addr)
+	case LDY:
+		ldy(cpu.r, cpu.memory, addr)
+	case STA:
+		sta(cpu.r, cpu.memory, addr)
+	case STX:
+		stx(cpu.r, cpu.memory, addr)
+	case STY:
+		sty(cpu.r, cpu.memory, addr)
+	case TAX:
+		tax(cpu.r)
+	case TAY:
+		tay(cpu.r)
+	case TSX:
+		tsx(cpu.r)
+	case TXA:
+		txa(cpu.r)
+	case TXS:
+		txs(cpu.r)
+	case TYA:
+		tya(cpu.r)
+	case ADC:
+		adc(cpu.r, cpu.memory, addr)
+	case AND:
+		and(cpu.r, cpu.memory, addr)
+	case ASL:
+		if opcode.Mode == accumulator {
+			aslAcc(cpu.r)
+		} else {
+			asl(cpu.r, cpu.memory, addr)
+		}
+	case BIT:
+		bit(cpu.r, cpu.memory, addr)
+	case CMP:
+		cmp(cpu.r, cpu.memory, addr)
+	case CPX:
+		cpx(cpu.r, cpu.memory, addr)
+	case CPY:
+		cpy(cpu.r, cpu.memory, addr)
+	case DEC:
+		dec(cpu.r, cpu.memory, addr)
+	case DEX:
+		dex(cpu.r)
+	case DEY:
+		dey(cpu.r)
+	case EOR:
+		eor(cpu.r, cpu.memory, addr)
+	case INC:
+		inc(cpu.r, cpu.memory, addr)
+	case INX:
+		inx(cpu.r)
+	case INY:
+		iny(cpu.r)
+	case LSR:
+		if opcode.Mode == accumulator {
+			lsrAcc(cpu.r)
+		} else {
+			lsr(cpu.r, cpu.memory, addr)
+		}
+	case ORA:
+		ora(cpu.r, cpu.memory, addr)
+	case ROL:
+		if opcode.Mode == accumulator {
+			rolAcc(cpu.r)
+		} else {
+			rol(cpu.r, cpu.memory, addr)
+		}
+	case ROR:
+		if opcode.Mode == accumulator {
+			rorAcc(cpu.r)
+		} else {
+			ror(cpu.r, cpu.memory, addr)
+		}
+	case SBC:
+		sbc(cpu.r, cpu.memory, addr)
+	case PHA:
+		pha(cpu.r, cpu.memory)
+	case PHP:
+		php(cpu.r, cpu.memory)
+	case PLA:
+		pla(cpu.r, cpu.memory)
+	case PLP:
+		plp(cpu.r, cpu.memory)
+	case JMP:
+		jmp(cpu.r, addr)
+	case JSR:
+		jsr(cpu.r, cpu.memory, addr)
+	case RTS:
+		rts(cpu.r, cpu.memory)
+	case RTI:
+		rti(cpu.r, cpu.memory)
+	case BCC:
+		additionalCycle += bcc(cpu.r, addr)
+	case BCS:
+		additionalCycle += bcs(cpu.r, addr)
+	case BEQ:
+		additionalCycle += beq(cpu.r, addr)
+	case BMI:
+		additionalCycle += bmi(cpu.r, addr)
+	case BNE:
+		additionalCycle += bne(cpu.r, addr)
+	case BPL:
+		additionalCycle += bpl(cpu.r, addr)
+	case BVC:
+		additionalCycle += bvc(cpu.r, addr)
+	case BVS:
+		additionalCycle += bvs(cpu.r, addr)
+	case CLC:
+		clc(cpu.r)
+	case CLD:
+		cld(cpu.r)
+	case CLI:
+		cli(cpu.r)
+	case CLV:
+		clv(cpu.r)
+	case SEC:
+		sec(cpu.r)
+	case SED:
+		sed(cpu.r)
+	case SEI:
+		sei(cpu.r)
+	case BRK:
+		brk(cpu.r, cpu.memory)
+	case NOP:
+	default:
+		panic("Unable to reach here")
+	}
 
-	// addr, pageCrossed := fetchOperand(cpu.r, cpu.memory, opcode.Mode)
-
-	// execute(opcode)
-
-	return 0
+	return opcode.Cycle + additionalCycle
 }
 
 func fetch(r *cpuRegister, m MemoryReader) byte {
