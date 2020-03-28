8ef0b9bc6c711291c41ab6c0b0bce1b412ff050b
ichirin2501 <ichirin.2501@gmail.com>
2020-03-28 22:43:18 +0900

    ppu reg Controller,Mask

--- nes/register.go
+++ nes/register.go
@@ -108,11 +108,83 @@ func (r *cpuRegister) UpdateNegativeFlag(val byte) {
 	r.SetNegativeFlag(val&0x80 != 0)
 }
 
-// TODO
+const (
+	_ byte = (1 << iota)
+	_
+	vRAMAddrIncrMask
+	spritePatternAddrMask
+	backgroundPatternAddrMask
+	spriteSizeMask
+	_
+	nmiOnVBlankMask
+)
+
+const (
+	grayscaleMask byte = (1 << iota)
+	showLeftBackgroundMask
+	showLeftSpritesMask
+	showBackgroundMask
+	showSpritesMask
+	emphaticRedMask
+	emphaticGreenMask
+	emphaticBlueMask
+)
+
 type ppuRegister struct {
+	Controller byte
+	Mask       byte
+	Status     byte
+	OAMAddr    byte
+	Scroll     uint16
+	Addr       uint16
+	Data       byte
 }
 
 // TODO
 func newPPURegister() *ppuRegister {
 	return &ppuRegister{}
+}
+
+func (p *ppuRegister) nameTableID() byte {
+	return p.Controller & 0x03
+}
+
+func (p *ppuRegister) ppuAddrIncrFlag() bool {
+	return (p.Controller & vRAMAddrIncrMask) == vRAMAddrIncrMask
+}
+
+func (p *ppuRegister) spritePatternAddrFlag() bool {
+	return (p.Controller & spritePatternAddrMask) == spritePatternAddrMask
+}
+
+func (p *ppuRegister) backgroundPatternAddrFlag() bool {
+	return (p.Controller & backgroundPatternAddrMask) == backgroundPatternAddrMask
+}
+
+func (p *ppuRegister) spriteSizeFlag() bool {
+	return (p.Controller & spriteSizeMask) == spriteSizeMask
+}
+
+func (p *ppuRegister) nmiOnVBlankFlag() bool {
+	return (p.Controller & nmiOnVBlankMask) == nmiOnVBlankMask
+}
+
+func (p *ppuRegister) grayScaleEnable() bool {
+	return (p.Mask & grayscaleMask) != grayscaleMask
+}
+
+func (p *ppuRegister) showLeftBackground() bool {
+	return (p.Mask & showLeftBackgroundMask) == showLeftBackgroundMask
+}
+
+func (p *ppuRegister) showLeftSprites() bool {
+	return (p.Mask & showLeftSpritesMask) == showLeftSpritesMask
+}
+
+func (p *ppuRegister) showBackground() bool {
+	return (p.Mask & showBackgroundMask) == showBackgroundMask
+}
+
+func (p *ppuRegister) showSprites() bool {
+	return (p.Mask & showSpritesMask) == showSpritesMask
 }
